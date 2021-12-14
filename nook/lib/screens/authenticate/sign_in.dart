0daef15a0adc6b9512a6bdb83649d8d5dc2037d1
Sejung <49921709+sejungk@users.noreply.github.com>
2021-12-13 20:23:48 -0500

    feat: authentication anonymous sign in button showing

---
+++ nook/lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.brown[100],
+      appBar: AppBar(
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        title: Text('Sign in to Nook'),
+      ),
+      body: Container(
+          padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+          child: ElevatedButton(
+              child: Text('Sign in Anon'), onPressed: () async {})),
+    );
+  }
+}
